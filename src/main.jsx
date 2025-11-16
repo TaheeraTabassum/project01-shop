d505f97279aa814b27f7efc7b04252d118e1c13d
TaheeraTabassum <tahiratabassumfarha@gmail.com>
2025-11-15 19:25:57 -0800

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+
+import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './Root';
+import Home from './page/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        index:true,
+        element:<Home></Home>
+      }
+    ]
+  },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+);
---
+++ src/shared/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TopHeader from './TopHeader'
+import Nav from './nav'
+
+export default function Header() {
+  return (
+    <div>
+        <TopHeader></TopHeader>
+        <div>
+            header
+        </div>
+        <Nav></Nav>
+    </div>
+  )
+}
